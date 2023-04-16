8afc0fea6fc117974a445a3e7665982d75f42395
maharekala <maha.rekala@gmail.com>
2023-04-16 12:12:30 -0700

    src code

---
+++ app.py
@@ -0,0 +1,19 @@
+import psutil
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def indexofapp():
+    cpu_percent = psutil.cpu_percent()
+    mem_percent = psutil.virtual_memory().percent
+    Message = None
+    if cpu_percent > 80 or mem_percent > 80:
+        Message = "High CPU or Memory Detected, scale up!!!"
+    return f"CPU Utilization: {cpu_percent} and MemoryUtilization: {mem_percent}"
+
+
+if __name__ == '__main__':
+  app.run(debug=True, host ='0.0.0.0')
+
